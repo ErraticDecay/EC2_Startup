f48b385ddc05a116926b11bc3a7579db0028d006
ErraticDecay <erraticdecay@gmail.com>
2020-02-17 17:09:25 -0500

    Update postgres_test.py

--- postgres_test.py
+++ postgres_test.py
@@ -2,14 +2,10 @@
 import psycopg2
 try:
     connect_str = "dbname='testpython' user='ec2-user' host='localhost' password='sean'"
-    # use our connection values to establish a connection
     conn = psycopg2.connect(connect_str)
-    # create a psycopg2 cursor that can execute queries
     cursor = conn.cursor()
-    # create a new table with a single column called "name"
-    cursor.execute("""CREATE TABLE tutorials (name char(40));""")
-    # run a SELECT statement - no data in there, but we can try it
-    cursor.execute("""SELECT * from tutorials""")
+    cursor.execute("""CREATE TABLE wes_test (name char(40));""")
+    cursor.execute("""SELECT * from wes_test""")
     conn.commit() # <--- makes sure the change is shown in the database
     rows = cursor.fetchall()
     print(rows)
